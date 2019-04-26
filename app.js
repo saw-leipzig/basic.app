a82b462dfe8027b3d7e2eb6edbbdae7e8eb7035d
Uwe Kretschmer <kretschmer@saw-leipzig.de>
2019-04-26 17:15:04 +0200

    Prepare release 1.0.11.

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 // Global application object
 // TODO: reimplement, so that everything important is accessable via the global app object
 function BasicApp (ctx, cfg) {
-    this.version = '1.0.10';
+    this.version = '1.0.11';
     this.context = ctx;
     this.config = cfg;
     this.plugins = [];  // Container for registered plugins
